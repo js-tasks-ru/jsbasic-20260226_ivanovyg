cb31f57c4c9effa2c02c01a04a0926570d4e717f
ivanovyg <thuglife83@mail.ru>
2026-03-18 22:23:44 +0300

    Решил задачу ''Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.rows) {
+    // Определить присутствует ли аттрибут
+    if (row.cells[3].hasAttribute('data-available')) {
+      // Проверить значение аттрибута и назанчить стиль строке
+      if (row.cells[3].dataset.available === 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
+    } else {
+      // Скрыть строку, если аттрибута нет
+      row.setAttribute('hidden', 'true');
+    }
+    
+    // Определить пол по значению ячейки и установить соответствующий стиль
+    if (row.cells[2].innerText === 'm') {
+      row.classList.add('male');    
+    } else if (row.cells[2].innerText === 'f') {
+      row.classList.add('female');        
+    }
+      
+    // Проверить возраст и вычеркнуть, если меньше положенного  
+    if (row.cells[1].innerText < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
